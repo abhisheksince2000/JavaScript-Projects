e979bbe908be49022676176a5a9049ba7be0550c
Abhishek vishwkarma <112538249+abhisheksince2000@users.noreply.github.com>
2024-10-22 08:52:05 +0000

    project 2 solution

--- project two/script.js
+++ project two/script.js
@@ -1,39 +1,3 @@
-# Projects Related to DOM
-## project link
-[click here](https://stackblitz.com/edit/stackblitz-starters-1sfkg7?description=HTML/CSS/JS%20Starter&file=script.js,styles.css,index.html&terminalHeight=10&title=Static%20Starter)
-
-# solution code
-## project 1
-``` javascript
-const buttons = document.querySelectorAll('.button')
-const body = document.querySelector('body')
-
-buttons.forEach(function(button) {
-  console.log(buttons);
-  button.addEventListener('click', function(e){
-    console.log(e)
-    console.log(e.target)
-    if(e.target.id === 'grey'){
-      body.style.background = e.target.id;
-    }
-    if(e.target.id === 'white'){
-      body.style.background = e.target.id;
-    }
-    if(e.target.id === 'blue'){
-      body.style.background = e.target.id;
-    }
-    if(e.target.id === 'yellow'){
-      body.style.background = e.target.id;
-    }
-
-  });
-});
-
-```
-
-## Project 2
-solution code 
-``` javascript
 // script.js
 
 function calculateBMI() {
@@ -81,5 +45,3 @@ function calculateBMI() {
 
     result.innerHTML = `Your BMI is ${bmi} (${suggestion})`;
 }
-
-```
